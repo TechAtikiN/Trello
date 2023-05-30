243e70e72e1e6d5c18ad78e83815c128911d83b2
techatikin <nikitakhabya03@gmail.com>
2023-05-30 14:18:17 +0530

    feat: appwrite config

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,20 @@
 'use client'
+
 import { FC } from 'react'
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/outline'
+
 import Image from 'next/image'
 import Avatar from 'react-avatar'
 
 const Header: FC = () => {
   return (
     <header>
-
+      {/* bg gradient  */}
       <div
         className='absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-purple-500 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50'
       />
 
+      {/* nav  */}
       <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
         <Image
           src='https://links.papareact.com/c2cdd5'
@@ -33,6 +36,8 @@ const Header: FC = () => {
           <Avatar name='Nikita Khabya' round size='50' color='#0055D1' />
         </div>
       </div>
+
+      {/* gpt banner  */}
       <div className='flex items-center justify-center px-5 py-2 md:py-5'>
         <p
           className='flex items-center text-sm font-light p-5 pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]'
